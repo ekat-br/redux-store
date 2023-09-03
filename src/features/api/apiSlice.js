b3ad010dfcc91759d8054f1bd526aaef38e8a5d2
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-09-03 17:29:01 +0200

    Improve fetching

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,10 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com'}),
+  baseQuery: (fetchBaseQuery({ baseUrl: '/'})), //naseURL /, da die lokale Datei im Root-Verzeichnis des Projektes liegt
   endpoints: builder => ({
     getAllProducts: builder.query({
-      query: () => '/products'
+      query: () => 'data/products.json'
     })
   })
 })
